10225574cf0397017964333f8c9915b6dab0e3cd
Your Name <you@example.com>
2024-09-19 16:31:22 +0530

    first react letter cal proj

---
+++ wordletter-counter/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import WordLetterCount from './wordLetterCount.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 id="top"> 
+            Mini project 1
+            </h1> 
+            <h1> 
+            Words and Letters 
+            Counter 
+            </h1> 
+            <wordLetterCounter /> 
+    </div>
+  );
+}
+
+export default App ;
