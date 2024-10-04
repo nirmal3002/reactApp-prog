e1d352ddaca0dd24d5a995086cb411d9587ae892
nirmal3002 <nirmal.m3002@gmail.com>
2024-10-04 09:53:53 +0000

    the read operation on react

--- react-router-lab/src/components/PersonList.js
+++ react-router-lab/src/components/PersonList.js
@@ -1,23 +1,63 @@
-import React from 'react';
-//import personadd from './PersonAdd"
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-//import notification from 
+import Notification from './Notification';
+
 import '../styles/PersonList.css'
 
 const API_URL=process.env.REACT_APP_API_URL
-const output = axios.get("https://5000-nirmal3002-reactappprog-h48tp35w1ha.ws-us116.gitpod.io/persons");
+const output = axios.get("https://5000-nirmal3002-reactappprog-ssm6vikaslb.ws-us116.gitpod.io/persons");
 
 
 
 const PersonList = () => {
-  console.log(output);
-  console.log(API_URL)
+    const [people, setPeople] = useState([]);
+    const [notification, setNotification] = useState('');
+//   console.log(output);
+//   console.log(API_URL)
 
-    return(
-        <div className='person-list'>
-            <h2>to add list of  the person</h2>
-        </div>
-    )
+useEffect(() => {
+    const fetchPeople = async () => {
+      try {
+        const response = await axios.get(API_URL);
+        setPeople(response.data);
+      } catch (error) {
+        console.error('Error fetching people:', error);
+      }
+    };
+    fetchPeople();
+  }, []);
 
-}
+
+  return (
+    <div className="person-list">
+      <h1>Person List</h1>
+      <Link to="/add" className="btn btn-add add-person-button">Add Person</Link>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {people.map(person => (
+            <tr key={person.id}>
+              <td>
+                <Link to={`/person/${person.id}`} className="person-name">
+                  {person.name}
+                </Link>
+              </td>
+              <td>{person.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {notification && (
+        <Notification message={notification} onClose={() => setNotification('')} />
+      )}
+    </div>
+  );
+};
+
 export default PersonList;
