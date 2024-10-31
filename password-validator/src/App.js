23365c5dd354754d42de3e00b1955500f8934ee0
nirmal3002 <nirmal.m3002@gmail.com>
2024-10-31 16:54:39 +0000

    the complete password-validator

--- password-validator/src/App.js
+++ password-validator/src/App.js
@@ -1,38 +1,70 @@
+// import React, { useState } from "react"; 
+// import validator from 'validator'
+// import './App.css'
+// const App = () => { 
+
+// 	const [errorMessage, setErrorMessage] = useState('') 
+
+// 	const validate = (value) => { 
+
+// 		if (validator.isStrongPassword(value, { 
+// 			minLength: 8, minLowercase: 1, 
+// 			minUppercase: 1, minNumbers: 1, minSymbols: 1 
+// 		})) { 
+// 			setErrorMessage('Is Strong Password') 
+// 		} else { 
+// 			setErrorMessage('Is Not Strong Password') 
+// 		} 
+// 	} 
+
+// 	return ( 
+// 		<div> 
+// 			<pre> 
+// 				<h2>Checking Password Strength in ReactJS</h2> 
+// 				<span>Enter Password: </span><input type="text"
+// 					onChange={(e) => validate(e.target.value)}></input> <br /> 
+// 				{errorMessage === '' ? null : 
+// 					<span >{errorMessage}</span>} 
+// 			</pre> 
+// 		</div> 
+// 	); 
+// } 
+
+// export default App
+
 import React, { useState } from "react"; 
-import validator from 'validator'
+import validator from 'validator';
+import './App.css';
 
 const App = () => { 
+    const [errorMessage, setErrorMessage] = useState('');
 
-	const [errorMessage, setErrorMessage] = useState('') 
+    const validate = (value) => { 
+        if (validator.isStrongPassword(value, { 
+            minLength: 8, minLowercase: 1, 
+            minUppercase: 1, minNumbers: 1, minSymbols: 1 
+        })) { 
+            setErrorMessage('Strong Password'); 
+        } else { 
+            setErrorMessage('Not a Strong Password'); 
+        } 
+    };
 
-	const validate = (value) => { 
+    return ( 
+        <div className="container"> 
+            <h2>Checking Password Strength in ReactJS</h2>
+            <span>Enter Password:</span>
+            <input
+                type="text"
+                onChange={(e) => validate(e.target.value)}
+            />
+            {errorMessage && (
+                <span className={errorMessage === 'Strong Password' ? 'success-message' : 'error-message'}>
+                    {errorMessage}
+                </span>
+            )}
+        </div>
+    ); 
+}; 
 
-		if (validator.isStrongPassword(value, { 
-			minLength: 8, minLowercase: 1, 
-			minUppercase: 1, minNumbers: 1, minSymbols: 1 
-		})) { 
-			setErrorMessage('Is Strong Password') 
-		} else { 
-			setErrorMessage('Is Not Strong Password') 
-		} 
-	} 
-
-	return ( 
-		<div style={{ 
-			marginLeft: '200px', 
-		}}> 
-			<pre> 
-				<h2>Checking Password Strength in ReactJS</h2> 
-				<span>Enter Password: </span><input type="text"
-					onChange={(e) => validate(e.target.value)}></input> <br /> 
-				{errorMessage === '' ? null : 
-					<span style={{ 
-						fontWeight: 'bold', 
-						color: 'red', 
-					}}>{errorMessage}</span>} 
-			</pre> 
-		</div> 
-	); 
-} 
-
-export default App
+export default App;
